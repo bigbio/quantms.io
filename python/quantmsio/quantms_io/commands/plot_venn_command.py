3919101b6054d8ff5bcac64b272b1b99695ed72d
zprobot <1727697083@qq.com>
2023-09-15 14:55:10 +0800

    venn-change

--- python/quantmsio/quantms_io/commands/plot_venn_command.py
+++ python/quantmsio/quantms_io/commands/plot_venn_command.py
@@ -1,17 +1,17 @@
 import click
 from quantms_io.core.tools import plot_peptidoform_charge_venn,plot_sequence_venn
 @click.command(
-    "get message from a set of PSMS", short_help="Convert msstats/mztab to parquet file"
+    "get message from a set of PSMS", short_help="plot venn for a set of Psms"
 )
-@click.option('--parquet_list', nargs=-1, type=str, help='List of psm parquet path')
-@click.option('--label_list', nargs=-1, type=str, help='List of parquet label')
-def compare_set_of_psms(parquet_list,label_list):
+@click.option('--parquets', nargs=-1, type=str, help='List of psm parquet path')
+@click.option('--tags', nargs=-1, type=str, help='List of parquet label')
+def compare_set_of_psms(parquets,tags):
     '''
     :param parquet_list: a set of psm parquet path
     :param label_list: a set of psm label
     '''
-    if len(parquet_list) != len(label_list):
+    if len(parquets) != len(tags):
         raise click.UsageError("Please provide same length of parquet_list and label_list")
     
-    plot_peptidoform_charge_venn(parquet_list,label_list)
-    plot_sequence_venn(parquet_list,label_list)
+    plot_peptidoform_charge_venn(parquets,tags)
+    plot_sequence_venn(parquets,tags)
