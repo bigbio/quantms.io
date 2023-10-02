71d7ad0c9c2fcacedf08e3763e502ddf9257c47f
Yasset Perez-Riverol <ypriverol@gmail.com>
2023-10-02 06:20:59 +0100

    update small things

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'quantms'
+project = 'quantms.io'
 copyright = '2022, daichengxin, jpfeuffer, timosachenberg, ypriverol'
 author = 'daichengxin, jpfeuffer, timosachenberg, ypriverol'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
