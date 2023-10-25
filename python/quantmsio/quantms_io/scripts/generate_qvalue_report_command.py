0e8bb56011c42cb91498b4d943d4af3993beef21
Yasset Perez-Riverol <ypriverol@gmail.com>
2023-10-25 19:45:07 +0100

    minor comments

--- python/quantmsio/quantms_io/scripts/generate_qvalue_report_command.py
+++ python/quantmsio/quantms_io/scripts/generate_qvalue_report_command.py
@@ -1,5 +1,6 @@
 import click
 import pandas as pd
+import pyarrow.parquet as pq
 
 
 def read_large_parquet(parquet_path: str, batch_size: int = 100000):
@@ -9,18 +10,28 @@ def read_large_parquet(parquet_path: str, batch_size: int = 100000):
     :param batch_size: _description_, defaults to 100000
     :yield: _description_
     """
-    reg_df = pd.read_parquet(parquet_path, engine='pyarrow', columns=['protein_accessions', 'protein_global_qvalue', 'is_decoy', 'condition'])
-    return reg_df
+    parquet_file = pq.ParquetFile(parquet_path)
+    for batch in parquet_file.iter_batches(batch_size=batch_size):
+        batch_df = batch.to_pandas()
+        yield batch_df
 
 
 @click.command()
 @click.option("--feature_parquet", help="Parquet file with features", required=True)
 @click.option("--output_file", help="Output file", required=True)
-def extract_protein_qvalues(feature_parquet: str, output_file: str):
+@click.option("--accession", help="Accession", required=True)
+def extract_protein_qvalues(feature_parquet: str, output_file: str, accession: str):
 
-    df_qvalues = read_large_parquet(feature_parquet)
-    df_qvalues.drop_duplicates(inplace=True)
-    df_qvalues.to_csv(output_file, index=False, mode='w')
+    chunks = read_large_parquet(feature_parquet)
+    q_df = pd.DataFrame()
+    for feature_df in chunks:
+        feature_df = feature_df[['protein_accessions', 'protein_global_qvalue', 'is_decoy', 'condition']]
+        feature_df = feature_df.explode('protein_accessions')
+        q_df = pd.concat([q_df, feature_df])
+        q_df.drop_duplicates(inplace=True)
+        q_df['dataset_accession'] = accession
+    print(q_df)
+    q_df.to_csv(output_file, index=False, mode="w")
 
 if __name__ == '__main__':
     try:
