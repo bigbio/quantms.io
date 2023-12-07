9405f2f49b3281a78b70660c1e79d23373220525
Yasset Perez-Riverol <ypriverol@gmail.com>
2023-12-07 19:51:31 +0000

    major refactoring

--- python/quantmsio/quantms_io/tests/test_statistics.py
+++ python/quantmsio/quantms_io/tests/test_statistics.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
-from quantms_io.core.statistics import ParquetStatistics
+from quantms_io.core.statistics import ParquetStatistics, IbaqStatistics
 
 
 class TestStatistics(TestCase):
 
-    def test_get_number_of_peptides(self):
+    def test_feature_get_number_of_peptides(self):
         # Mock the duckdb.sql method to return a DataFrame with a single column 'sequence'
         parquet_path = "/Users/yperez/work/quantms-data/PXD037340.2/PXD037340.2-93425767-e297-4e8e-b8dd-30793e3aae19.feature.parquet"
         # Create an instance of ParquetStatistics
@@ -24,3 +24,17 @@ class TestStatistics(TestCase):
         assert sample_accession_count == 31
         assert proteins_count == 5578
         assert msruns_count == 110
+
+    def test_ibaq_get_number_of_proteins(self):
+        # Mock the duckdb.sql method to return a DataFrame with a single column 'sequence'
+        ibaq_path = "/Users/yperez/work/PXD000561-protein-ibaq.tsv"
+        # Create an instance of ParquetStatistics
+        ibaq_stats = IbaqStatistics(ibaq_path)
+
+        # Call the method get_number_of_peptides
+        protein_count = ibaq_stats.get_number_of_proteins()
+        sample_accession_count = ibaq_stats.get_number_of_samples()
+
+        # Assert that the result is equal to the number of unique peptides in the DataFrame
+        assert protein_count == 12025
+        assert sample_accession_count == 85
