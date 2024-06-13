1c7132be45bcd77c1a7361a8c3922ea8f03bf2f7
zprobot <1727697083@qq.com>
2024-06-13 20:05:08 +0800

    update: doc,query

--- quantmsio/core/query.py
+++ quantmsio/core/query.py
@@ -65,41 +65,45 @@ class Parquet:
         else:
             raise FileNotFoundError(f"the file {parquet_path} does not exist.")
 
-    def get_report_from_database(self, runs: list):
+    def get_report_from_database(self, runs: list,columns: list = None):
         """
         This function loads the report from the duckdb database for a group of ms_runs.
         :param runs: A list of ms_runs
         :return: The report
         """
+        cols = ", ".join(columns) if columns and isinstance(columns, list) else "*"
         database = self.parquet_db.sql(
             """
-            select * from parquet_db
+            select {} from parquet_db
             where reference_file_name IN {}
             """.format(
+                cols,
                 tuple(runs)
             )
         )
         report = database.df()
         return report
 
-    def get_samples_from_database(self, samples: list):
+    def get_samples_from_database(self, samples: list,columns: list = None):
         """
         This function loads the report from the duckdb database for a group of samples.
         :param runs: A list of samples
         :return: The report
         """
+        cols = ", ".join(columns) if columns and isinstance(columns, list) else "*"
         database = self.parquet_db.sql(
             """
-            select * from parquet_db
+            select {} from parquet_db
             where sample_accession IN {}
             """.format(
+                cols,
                 tuple(samples)
             )
         )
         report = database.df()
         return report
 
-    def iter_samples(self, file_num: int = 20):
+    def iter_samples(self, file_num: int = 20,columns: list = None):
         """
         :params file_num: The number of files being processed at the same time(default 10)
         :yield: _description_
@@ -107,20 +111,20 @@ class Parquet:
         samples = self.get_unique_samples()
         ref_list = [samples[i : i + file_num] for i in range(0, len(samples), file_num)]
         for refs in ref_list:
-            batch_df = self.get_report_from_database(refs)
+            batch_df = self.get_report_from_database(refs,columns)
             yield refs, batch_df
 
-    def iter_chunk(self, batch_size: int = 500000):
+    def iter_chunk(self, batch_size: int = 500000,columns: list = None):
         """_summary_
         :param batch_size: _description_, defaults to 100000
         :yield: _description_
         """
         parquet_file = pq.ParquetFile(self._path)
-        for batch in parquet_file.iter_batches(batch_size=batch_size):
+        for batch in parquet_file.iter_batches(batch_size=batch_size,columns=columns):
             batch_df = batch.to_pandas()
             yield batch_df
 
-    def iter_file(self, file_num: int = 10):
+    def iter_file(self, file_num: int = 10,columns: list = None):
         """
         :params file_num: The number of files being processed at the same time(default 10)
         :yield: _description_
@@ -128,7 +132,7 @@ class Parquet:
         references = self.get_unique_references()
         ref_list = [references[i : i + file_num] for i in range(0, len(references), file_num)]
         for refs in ref_list:
-            batch_df = self.get_report_from_database(refs)
+            batch_df = self.get_report_from_database(refs,columns)
             yield batch_df
 
     def inject_spectrum_msg(self, df: pd.DataFrame, mzml_directory: str):
