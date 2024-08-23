c4b1d13c0b21d70ce763688a4946f93cbcc9d9d1
zprobot <1727697083@qq.com>
2024-08-23 23:43:19 +0800

    feat：maxquant

---
+++ quantmsio/commands/maxquant_command.py
@@ -0,0 +1,49 @@
+import click
+from quantmsio.core.maxquant_convert import MaxquantConvert
+from quantmsio.core.project import create_uuid_filename
+
+@click.command("convert-maxquant", short_help="Convert msstats/mztab to parquet file")
+@click.option(
+    "--sdrf_file",
+    help="the SDRF file needed to extract some of the metadata",
+    required=True,
+)
+@click.option(
+    "--evidence_file",
+    help="the evidence file from maxquant",
+    required=True,
+)
+@click.option(
+    "--output_folder",
+    help="Folder where the parquet file will be generated",
+    required=True,
+)
+@click.option(
+    "--output_prefix_file",
+    help="Prefix of the parquet file needed to generate the file name",
+    required=False,
+)
+def convert_maxquant(
+    sdrf_file: str,
+    evidence_file: str,
+    output_folder: str,
+    output_prefix_file: str = None,
+):
+    """
+    convert maxquant evidence to a parquet file. The parquet file will contain the features and the metadata.
+    :param sdrf_file: the SDRF file needed to extract some of the metadata
+    :param evidence_file: the evidence file from maxquant
+    :param output_folder: Folder where the Json file will be generated
+    :param output_prefix_file: Prefix of the Json file needed to generate the file name
+    :return: none
+    """
+
+    if evidence_file is None or output_folder is None or sdrf_file is None:
+        raise click.UsageError("Please provide all the required parameters")
+
+    if not output_prefix_file:
+        output_prefix_file = ""
+
+    maxquant_manager = MaxquantConvert()
+    output_path = output_folder + "/" + create_uuid_filename(output_prefix_file, ".psm.parquet")
+    maxquant_manager.convert_to_parquet(evidence_file,sdrf_file,output_path)
--- quantmsio/quantmsioc.py
+++ quantmsio/quantmsioc.py
@@ -6,6 +6,7 @@ Commandline interface for quantmsio package allows generating the quantms.io fil
 import click
 
 from quantmsio import __version__ as __version__
+from quantmsio.commands.maxquant_command import convert_maxquant
 from quantmsio.commands.absolute_expression_command import convert_ibaq_absolute
 from quantmsio.commands.attach_file_command import attach_file_to_json
 from quantmsio.commands.convert_tsv_to_json_command import json
@@ -68,7 +69,7 @@ cli.add_command(generate_pride_project_json)
 cli.add_command(json)
 cli.add_command(generate_report_about_project)
 cli.add_command(map_gene_msg_to_parquet)
-
+cli.add_command(convert_maxquant)
 
 def quantms_io_main():
     """
