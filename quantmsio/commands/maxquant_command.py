a98233fcc38d24b4d713ced2c833c2c39e07a336
zprobot <1727697083@qq.com>
2024-10-31 19:18:25 +0800

    update: command

--- quantmsio/commands/maxquant_command.py
+++ quantmsio/commands/maxquant_command.py
@@ -5,11 +5,62 @@ from quantmsio.core.project import create_uuid_filename
 
 @click.command(
     "convert-maxquant-psm",
-    short_help="Convert psm from mzTab to parquet file in quantms io",
+    short_help="Convert psm from msms.txt to parquet file in quantms.io",
 )
 @click.option(
     "--msms_file",
     help="the msms.txt file, this will be used to extract the peptide information",
+    required=True,
+)
+@click.option(
+    "--output_folder",
+    help="Folder where the parquet file will be generated",
+    required=True,
+)
+@click.option(
+    "--chunksize",
+    help="Read batch size",
+    default=1000000,
+)
+@click.option(
+    "--output_prefix_file",
+    help="Prefix of the parquet file needed to generate the file name",
+    required=False,
+)
+def convert_maxquant_psm(
+    msms_file: str,
+    output_folder: str,
+    chunksize: int,
+    output_prefix_file: str,
+):
+    """
+    convert maxquant psm section to a parquet file.
+    :param msms_file: the msms.txt file, this will be used to extract the peptide information
+    :param output_folder: Folder where the Json file will be generated
+    :param chunksize: Read batch size
+    :param output_prefix_file: Prefix of the Json file needed to generate the file name
+    """
+
+    if msms_file is None or output_folder is None:
+        raise click.UsageError("Please provide all the required parameters")
+
+    if not output_prefix_file:
+        output_prefix_file = ""
+
+    MQ = MaxQuant()
+    output_path = output_folder + "/" + create_uuid_filename(output_prefix_file, ".psm.parquet")
+    MQ.convert_psm_to_parquet(msms_path=msms_file, output_path=output_path, chunksize=chunksize)
+
+
+
+
+@click.command(
+    "convert-maxquant-feature",
+    short_help="Convert feature from evidence to parquet file in quantms.io",
+)
+@click.option(
+    "--evidence_file",
+    help="the evidence.txt file, this will be used to extract the peptide information",
     required=True,
 )
 @click.option(
@@ -32,8 +83,8 @@ from quantmsio.core.project import create_uuid_filename
     help="Prefix of the parquet file needed to generate the file name",
     required=False,
 )
-def convert_maxquant_psm(
-    msms_file: str,
+def convert_maxquant_feature(
+    evidence_file: str,
     sdrf_file: str,
     output_folder: str,
     chunksize: int,
@@ -41,19 +92,19 @@ def convert_maxquant_psm(
 ):
     """
     convert mztab psm section to a parquet file. The parquet file will contain the features and the metadata.
-    :param msms_file: the msms.txt file, this will be used to extract the peptide information
+    :param evidence_file: the msms.txt file, this will be used to extract the peptide information
     :param sdrf_file: the SDRF file needed to extract some of the metadata
     :param output_folder: Folder where the Json file will be generated
     :param chunksize: Read batch size
     :param output_prefix_file: Prefix of the Json file needed to generate the file name
     """
 
-    if msms_file is None or output_folder is None:
+    if evidence_file is None or sdrf_file is None or output_folder is None:
         raise click.UsageError("Please provide all the required parameters")
 
     if not output_prefix_file:
         output_prefix_file = ""
 
-    MQ = MaxQuant(sdrf_file, msms_file)
+    MQ = MaxQuant()
     output_path = output_folder + "/" + create_uuid_filename(output_prefix_file, ".psm.parquet")
-    MQ.convert_to_parquet(output_path=output_path, chunksize=chunksize)
+    MQ.convert_feature_to_parquet(evidence_path=evidence_file, sdrf_path=sdrf_file, output_path=output_path, chunksize=chunksize)
--- quantmsio/quantmsioc.py
+++ quantmsio/quantmsioc.py
@@ -18,7 +18,7 @@ from quantmsio.commands.generate_spectra_message_command import map_spectrum_mes
 from quantmsio.commands.plot_command import plot
 from quantmsio.commands.statistic_command import statistics
 from quantmsio.commands.pg_matrix_command import convert_pg_matrix
-from quantmsio.commands.maxquant_command import convert_maxquant_psm
+from quantmsio.commands.maxquant_command import convert_maxquant_psm, convert_maxquant_feature
 from quantmsio.commands.ibaq_command import convert_ibaq_file
 
 
@@ -49,6 +49,7 @@ cli.add_command(plot)
 cli.add_command(statistics)
 cli.add_command(convert_pg_matrix)
 cli.add_command(convert_maxquant_psm)
+cli.add_command(convert_maxquant_feature)
 cli.add_command(convert_ibaq_file)
 
 # cli.add_command(convert_maxquant)
