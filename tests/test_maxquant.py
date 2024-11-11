5a0b0d158ef9ca3cf87c7b619e6310321c305011
zprobot <1727697083@qq.com>
2024-11-11 10:04:32 +0800

    update: test

--- tests/test_maxquant.py
+++ tests/test_maxquant.py
@@ -1,4 +1,6 @@
+from quantmsio.core.feature import Feature
 from quantmsio.core.maxquant import MaxQuant
+from quantmsio.core.sdrf import SDRFHandler
 from .common import datafile
 from unittest import TestCase
 from quantmsio.core.psm import Psm
@@ -11,15 +13,29 @@ class TestFeatureHandler(TestCase):
     test_datas = [
         (
             "maxquant/msms.txt",
+            "maxquant/evidence.txt",
             "maxquant/sdrf.tsv",
         ),
     ]
 
     @data(*test_datas)
-    def test_transform_maxquant(self, test_data):
+    def test_transform_psm(self, test_data):
         msms_file = datafile(test_data[0])
         M = MaxQuant()
         for df in M.iter_batch(msms_file, "psm", chunksize=500000):
             M.transform_psm(df)
             Psm.convert_to_parquet_format(df)
             Psm.transform_parquet(df)
+
+    @data(*test_datas)
+    def test_transform_feature(self, test_data):
+        evidence_file = datafile(test_data[1])
+        sdrf_file = datafile(test_data[2])
+        Sdrf = SDRFHandler(sdrf_file)
+        M = MaxQuant()
+        M.experiment_type = Sdrf.get_experiment_type_from_sdrf()
+        M._sample_map = Sdrf.get_sample_map_run()
+        for df in M.iter_batch(evidence_file, chunksize=500000):
+            M.transform_feature(df)
+            Feature.convert_to_parquet_format(df)
+            Feature.transform_feature(df)
