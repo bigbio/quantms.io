fce1ba2eb273c13303058f09b36dec037df98f06
Yasset Perez-Riverol <ypriverol@gmail.com>
2025-03-22 17:06:04 +0100

    small changes in the code for optimization, more tests

--- tests/test_diann.py
+++ tests/test_diann.py
@@ -1,20 +1,21 @@
-import pytest
-from .common import datafile
+from pathlib import Path
+
 from quantmsio.core.diann import DiaNNConvert
 from quantmsio.core.feature import Feature
 
+TEST_DATA_ROOT = Path(__file__).parent / "examples"
 
 TEST_DATA = (
-    "DIANN/diann_report.tsv",
-    "DIANN/PXD019909-DIA.sdrf.tsv",
-    "DIANN/mzml",
+    TEST_DATA_ROOT / "DIANN/diann_report.tsv",
+    TEST_DATA_ROOT / "DIANN/PXD019909-DIA.sdrf.tsv",
+    TEST_DATA_ROOT / "DIANN/mzml",
 )
 
 
 def test_transform_feature():
-    report_file = datafile(TEST_DATA[0])
-    sdrf_file = datafile(TEST_DATA[1])
-    mzml = datafile(TEST_DATA[2])
+    report_file = TEST_DATA[0]
+    sdrf_file = TEST_DATA[1]
+    mzml = TEST_DATA[2]
     D = DiaNNConvert(report_file, sdrf_file)
     for report in D.main_report_df(0.05, mzml, 2):
         D.add_additional_msg(report)
@@ -23,9 +24,9 @@ def test_transform_feature():
 
 
 def test_transform_features():
-    report_file = datafile(TEST_DATA[0])
-    sdrf_file = datafile(TEST_DATA[1])
-    mzml = datafile(TEST_DATA[2])
+    report_file = TEST_DATA[0]
+    sdrf_file = TEST_DATA[1]
+    mzml = TEST_DATA[2]
     D = DiaNNConvert(report_file, sdrf_file)
     for report in D.main_report_df(0.05, mzml, 2):
         D.add_additional_msg(report)
